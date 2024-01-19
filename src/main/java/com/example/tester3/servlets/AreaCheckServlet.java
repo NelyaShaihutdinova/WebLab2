505d181ebdd364442b41ed0e20f3d799a776b272
NelyaShaihutdinova <fohado@mail.ru>
2024-01-19 18:26:21 +0300

    first commit

---
+++ src/main/java/com/example/tester3/servlets/AreaCheckServlet.java
@@ -0,0 +1,55 @@
+package com.example.tester3.servlets;
+
+import com.example.tester3.module.Point;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+import java.io.IOException;
+import java.util.*;
+
+@WebServlet("/areaChecker")
+public class AreaCheckServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        Enumeration<String> headers  = request.getHeaderNames();
+//        while (headers.hasMoreElements()){
+//            String name = headers.nextElement();
+//            String value = request.getHeader(name);
+//            System.out.println(name+" = "+value);
+//        }
+        HttpSession session = request.getSession(true);
+        try {
+            var x = Float.parseFloat(request.getParameter("X"));
+            var y = Float.parseFloat(request.getParameter("Y"));
+            var r = Integer.parseInt(request.getParameter("R"));
+            var point = new Point(x, y, r);
+            var bean = (List<Point>) session.getAttribute("array");
+            if (bean == null) {
+                bean = new ArrayList<Point>();
+                session.setAttribute("array", bean);
+            }
+            bean.add(0, point);
+            if ("withOutRedirect".equals(request.getParameter("action"))) {
+                var gson = new Gson();
+                Map<String, Object> json = new HashMap<>();
+                json.put("x", x);
+                json.put("y", y);
+                json.put("r", r);
+                json.put("result", point.getHit());
+                var msg = gson.toJson(json);
+                response.setContentType("application/json");
+                response.getWriter().write(msg);
+            } else if ("withRedirect".equals(request.getParameter("action"))) {
+                getServletContext().getRequestDispatcher("/result.jsp").forward(request, response);
+            }
+        } catch (Exception e) {
+            response.sendRedirect(request.getContextPath() + "/controller");
+        }
+    }
+}
---
+++ src/main/webapp/js/sendButton.js
@@ -0,0 +1,111 @@
+let button = document.getElementById('checkButton')
+
+button.onclick = handleButtonClick;
+table = document.getElementById("result");
+
+function handleButtonClick() {
+    let flag = true;
+    let option = document.getElementById("select").value;
+    let x;
+    let y;
+    let r;
+    if (option.includes(option)) {
+        x = option;
+    } else {
+        flag = false;
+        alert("x should be one of the following values: -3, -2, -1, 0, 1, 2, 3, 4, 5");
+    }
+
+    let coordinatesY = document.getElementById("inputText").value;
+    if (coordinatesY !== '') {
+        let checkY = Number(coordinatesY);
+        if (!isNaN(checkY)) {
+            if (3 > checkY && checkY > -5) {
+                y = checkY;
+            } else {
+                flag = false;
+                alert("y belongs to (-5; 3)");
+            }
+        } else {
+            flag = false;
+            alert("y should contain only digits");
+        }
+    } else {
+        flag = false;
+        alert("y cannot be empty");
+    }
+
+    let radius = document.querySelectorAll('input[type=radio]');
+    for (let i = 0; i < radius.length; i++) {
+        if (radius[i].checked) {
+            r = radius[i].value;
+            break;
+        }
+    }
+    if (flag) {
+        rValue = parseInt(r);
+        localStorage.setItem("rValue", rValue);
+        send(x, y, rValue);
+        redrawGraph(rValue);
+        redrawPoint();
+    }
+}
+
+async function send(x, y, r) {
+    console.log("SENDING");
+    const form = $('<form>', {
+        action: "controller",
+        method: "post"
+    });
+    const args = {X: x, Y: y, R: r};
+    Object.entries(args).forEach(entry => {
+        const [key, value] = entry;
+        $('<input>').attr({
+            type: "hidden",
+            name: key,
+            value: value
+        }).appendTo(form);
+    });
+    form.appendTo('body').submit()
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (!localStorage.getItem('rValue')) {
+        localStorage.setItem('rValue', "R")
+        rValue = "R"
+    } else {
+        rValue = localStorage.getItem('rValue')
+    }
+    redrawGraph(rValue)
+    redrawPoint()
+
+});
+function redrawPoint(){
+    const table = document.getElementById("result");
+    if (table) {
+        for (let item of table.rows) {
+            const x = parseFloat(item.children[0].innerText.trim());
+            const y = parseFloat(item.children[1].innerText.trim());
+            const r = parseFloat(item.children[2].innerText.trim());
+            if (isNaN(x) || isNaN(y) || isNaN(r)) continue;
+            drawPoint(x, y, item.children[3].innerText.trim() === "true");
+        }
+    }
+}
+$('input[name="radiobutton"]').on('change', function () {
+    let rBox = document.querySelectorAll('input[name=radiobutton]');
+    let countR = 0;
+    for (let i = 0; i < rBox.length; i++) {
+        let check = rBox[i];
+        if (check.checked) {
+            var r = check.value;
+            countR++;
+        }
+    }
+    if (countR === 1) {
+        rValue = parseInt(r)
+        localStorage.setItem("rValue", rValue)
+        redrawGraph(rValue)
+        redrawPoint()
+    }
+});
